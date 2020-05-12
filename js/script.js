9c78a3297e4bd0b67e0754d92afe098a238d9363
Sluggly <53790466+Sluggly@users.noreply.github.com>
2020-05-12 17:11:29 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -26,12 +26,15 @@ class Combo {
 }
 
 // Dictionnaire et listes
+var filtreList = [];
 var roleDict = [];
 var ninjaDict = [];
 var ninjaList = [];
 var statDict = [];
 var statImageDict = [];
 var typeImageDict = [];
+var filtreDict = [];
+var typeFiltreDict = [];
 
 // Initialisation des dictionnaires
 roleDict['DPS'] = [];
@@ -54,71 +57,91 @@ typeImageDict['Terre'] = 'src/Terre.png';
 typeImageDict['Foudre'] = 'src/Foudre.png';
 typeImageDict['Normal'] = 'src/Normal.png';
 typeImageDict['God'] = 'src/God.png';
+filtreDict['Force'] = 'F';
+filtreDict['Agilité'] = 'A';
+filtreDict['Intelligence'] = 'I';
+typeFiltreDict['Force'] = 'mainstat';
+typeFiltreDict['Agilité'] = 'mainstat';
+typeFiltreDict['Intelligence'] = 'mainstat';
+typeFiltreDict['Ranged'] = 'range';
+typeFiltreDict['Melee'] = 'range';
+typeFiltreDict['R'] = 'rarity';
+typeFiltreDict['SR'] = 'rarity';
+typeFiltreDict['SSR'] = 'rarity';
+typeFiltreDict['DPS'] = 'role';
+typeFiltreDict['TANK'] = 'role';
+typeFiltreDict['STUN'] = 'role';
+typeFiltreDict['BURST'] = 'role';
+typeFiltreDict['AOE'] = 'role';
+typeFiltreDict['SUPPORT'] = 'role';
 
 // Variables globales
 var page = document.getElementById("Page-Menu");
 
 
 // Création des ninja
-var Zabuza = createNinja('Zabuza',0,'A','Eau','Eau','R',['DPS'],'Zabuza.png');
-var Haku = createNinja('Haku',600,'A','Eau','Air','R',['DPS'],'Haku.png');
-var Kiba = createNinja('Kiba',0,'A','Terre','Terre','R',['DPS'],'Kiba.png');
-var Choji = createNinja('Choji',0,'F','Feu','Feu','R',['TANK'],'Choji.png');
-var Ino = createNinja('Ino Yamanaka',600,'A','Foudre','Foudre','R',['STUN'],'Ino.png');
-var Rin = createNinja('Rin',600,'I','Normal','Normal','R',['SUPPORT'],'Rin.png');
-var Shikamaru = createNinja('Shikamaru Nara',600,'I','Terre','Terre','R',['STUN', 'SUPPORT'],'Shikamaru.png');
-var Karin = createNinja('Karin',600,'A','Feu','Feu','R',['SUPPORT'],'Karin.png');
-var Sakura = createNinja('Sakura Haruno',600,'I','Normal','Normal','R',['SUPPORT'],'Sakura.png');
-var Tenten = createNinja('Tenten',525,'A','Normal','Air','R',['DPS'],'Tenten.png');
-var Rock = createNinja('Rock Lee',0,'A','Normal','Normal','R',['BURST'],'Rock.png');
-var Chiyo = createNinja('Grandma Chiyo',600,'I','Normal','Normal','R',['SUPPORT'],'Chiyo.png');
-var Orochimaru = createNinja('Orochimaru',600,'I','Air','Air','SR',['AOE'],'Orochimaru.png');
-var Hinata = createNinja('Hinata Hyuga',600,'A','Foudre','Foudre','SR',['DPS'],'Hinata.png');
-var Hidan = createNinja('Hidan',245,'F','Normal','Normal','SR',['TANK'],'Hidan.png');
-var Kakuzu = createNinja('Kakuzu',0,'F','Foudre','Foudre','SR',['DPS'],'Kakuzu.png');
-var Danzo = createNinja('Danzo',600,'A','Feu','Air','SR',['DPS', 'STUN'],'Danzo.png');
-var Kurenai = createNinja('Kurenai Yuhi',600,'I','Feu','Feu','SR',['SUPPORT', 'STUN'],'Kurenai.png');
-var Onoki = createNinja('Onoki',600,'I','Terre','Terre','SR',['AOE'],'Onoki.png');
-var Kurotsuchi = createNinja('Kurotsuchi',600,'A','Eau','Terre','SR',['STUN', 'SUPPORT'],'Kurotsuchi.png');
-var Kimimaro = createNinja('Kimimaro',0,'F','Normal','Normal','SR',['TANK', 'STUN'],'Kimimaro.png');
-var Jugo = createNinja('Jugo',0,'F','Normal','God','SR',['TANK', 'STUN'],'Jugo.png');
-var Kankuro = createNinja('Kankuro',600,'A','Foudre','Terre','SR',['DPS'],'Kankuro.png');
-var Narutoj = createNinja('Naruto Jinchuriky',0,'F','Air','Air','SR',['TANK'],'Narutoj.png');
-var Chojuro = createNinja('Chojuro',0,'A','Eau','Eau','SR',['BURST'],'Chojuro.png');
-var Shino = createNinja('Shino Aburame',600,'I','Terre','Terre','SR',['DPS'],'Shino.png');
-var Sasori = createNinja('Sasori',600,'A','Air','Air','SR',['AOE'],'Sasori.png');
-var Zetsu = createNinja('Zetsu',600,'A','Terre','Terre','SR',['DPS', 'SUPPORT'],'Zetsu.png');
+{
 var Asuma = createNinja('Asuma Sarutobi',0,'A','Air','Feu','SR',['DPS'],'Asuma.png');
+var Ay = createNinja('Ay',0,'F','Foudre','Foudre','SSR',['BURST'],'Ay.png');
+var Choji = createNinja('Choji',0,'F','Feu','Feu','R',['TANK'],'Choji.png');
+var Chojuro = createNinja('Chojuro',0,'A','Eau','Eau','SR',['BURST'],'Chojuro.png');
+var Chiyo = createNinja('Grandma Chiyo',600,'I','Normal','Normal','R',['SUPPORT'],'Chiyo.png');
+var Danzo = createNinja('Danzo',600,'A','Feu','Air','SR',['DPS', 'STUN'],'Danzo.png');
+var Deidara = createNinja('Deidara',600,'I','Terre','Terre','SR',['DPS'],'Deidara.png');
+var Gaara = createNinja('Gaara',600,'I','Air','Air','SR',['SUPPORT', 'STUN'],'Gaara.png');
+var Haku = createNinja('Haku',600,'A','Eau','Air','R',['DPS'],'Haku.png');
+var Hashirama = createNinja('Hashirama Senju',0,'F','Terre','Eau','SSR',['TANK', 'STUN'],'Hashirama.png');
+var Hidan = createNinja('Hidan',245,'F','Normal','Normal','SR',['TANK'],'Hidan.png');
+var Hinata = createNinja('Hinata Hyuga',600,'A','Foudre','Foudre','SR',['DPS'],'Hinata.png');
+var Hiruzen = createNinja('Hiruzen Sarutobi',600,'I','Terre','Eau','SR',['AOE'],'Hiruzen.png');
+var Ino = createNinja('Ino Yamanaka',600,'A','Foudre','Foudre','R',['STUN'],'Ino.png');
+var Itachi = createNinja('Itachi Uchiha',600,'I','Feu','Feu','SSR',['BURST', 'SUPPORT'],'Itachi.png');
+var Jiraya = createNinja('Jiraya',0,'F','Feu','Terre','SR',['TANK','STUN'],'Jiraya.png');
+var Jugo = createNinja('Jugo',0,'F','Normal','God','SR',['TANK', 'STUN'],'Jugo.png');
+var Kabuto = createNinja('Sage Kabuto',600,'I','God','Normal','SSR',['SUPPORT', 'STUN'],'Kabuto.png');
+var Kakashi = createNinja('Kakashi Hatake',0,'A','Foudre','Terre','SR',['STUN'],'Kakashi.png');
+var Karin = createNinja('Karin',600,'A','Feu','Feu','R',['SUPPORT'],'Karin.png');
+var Kakuzu = createNinja('Kakuzu',0,'F','Foudre','Foudre','SR',['DPS'],'Kakuzu.png');
+var Kankuro = createNinja('Kankuro',600,'A','Foudre','Terre','SR',['DPS'],'Kankuro.png');
+var Kiba = createNinja('Kiba',0,'A','Terre','Terre','R',['DPS'],'Kiba.png');
 var Killer = createNinja('Killer Bee',0,'A','Foudre','Eau','SR',['DPS'],'Killer.png');
+var Kimimaro = createNinja('Kimimaro',0,'F','Normal','Normal','SR',['TANK', 'STUN'],'Kimimaro.png');
 var Kisame = createNinja('Kisame Hoshigaki',0,'F','Eau','Eau','SR',['DPS', 'STUN'],'Kisame.png');
-var Suigetsu = createNinja('Suigetsu',0,'A','Eau','Eau','SR',['DPS', 'STUN'],'Suigetsu.png');
-var Temari = createNinja('Temari',600,'A','Air','Air','SR',['DPS'],'Temari.png');
+var Konan = createNinja('Konan',800,'A','Eau','Terre','SR',['AOE'],'Konan.png');
+var Kurenai = createNinja('Kurenai Yuhi',600,'I','Feu','Feu','SR',['SUPPORT', 'STUN'],'Kurenai.png');
+var Kurotsuchi = createNinja('Kurotsuchi',600,'A','Eau','Terre','SR',['STUN', 'SUPPORT'],'Kurotsuchi.png');
+var Kushina = createNinja('Kushina Uzumaki',600,'I','Eau','Eau','SR',['SUPPORT'],'Kushina.png');
+var Madara = createNinja('Madara Uchiha',0,'F','Feu','Feu','SSR',['DPS', 'STUN'],'Madara.png');
+var Maito = createNinja('Maito Gai',0,'F','Normal','Normal','SR',['BURST'],'Maito.png');
 var Mei = createNinja('Mei Terumi',600,'A','Eau','Eau','SR',['DPS', 'SUPPORT'],'Mei.png');
 var Minato = createNinja('Minato Namikaze',0,'A','Eau','Air','SSR',['ASSASSIN'],'Minato.png');
-var Kabuto = createNinja('Sage Kabuto',600,'I','God','Normal','SSR',['SUPPORT', 'STUN'],'Kabuto.png');
-var Madara = createNinja('Madara Uchiha',0,'F','Feu','Feu','SSR',['DPS', 'STUN'],'Madara.png');
-var Tsunade = createNinja('Tsunade',0,'F','Eau','Feu','SR',['TANK', 'STUN'],'Tsunade.png');
-var Itachi = createNinja('Itachi Uchiha',600,'I','Feu','Feu','SSR',['BURST', 'SUPPORT'],'Itachi.png');
-var Hashirama = createNinja('Hashirama Senju',0,'F','Terre','Eau','SSR',['TANK', 'STUN'],'Hashirama.png');
-var Maito = createNinja('Maito Gai',0,'F','Normal','Normal','SR',['BURST'],'Maito.png');
-var Kakashi = createNinja('Kakashi Hatake',0,'A','Foudre','Terre','SR',['STUN'],'Kakashi.png');
-var Obito = createNinja('Obito Uchiha',0,'A','Feu','Feu','SSR',['DPS', 'STUN'],'Obito.png');
-var Tobirama = createNinja('Tobirama Senju',0,'A','Eau','Eau','SR',['AOE'],'Tobirama.png');
-var Gaara = createNinja('Gaara',600,'I','Air','Air','SR',['SUPPORT', 'STUN'],'Gaara.png');
+var Narutoj = createNinja('Naruto Jinchuriky',0,'F','Air','Air','SR',['TANK'],'Narutoj.png');
 var Narutos = createNinja('Naruto Sage',0,'A','Air','Air','SSR',['DPS', 'STUN'],'Narutos.png');
-var Shisui = createNinja('Shisui Uchiha',0,'A','Feu','Feu','SSR',['BURST'],'Shisui.png');
-var Ay = createNinja('Ay',0,'F','Foudre','Foudre','SSR',['BURST'],'Ay.png');
-var Jiraya = createNinja('Jiraya',0,'F','Feu','Terre','SR',['TANK','STUN'],'Jiraya.png');
 var Neji = createNinja('Neji Hyuga',0,'A','Eau','Terre','SR',['BURST'],'Neji.png');
-var Sasuke = createNinja('Sasuke Uchiha',0,'A','Foudre','Foudre','SR',['AOE'],'Sasuke.png');
+var Obito = createNinja('Obito Uchiha',0,'A','Feu','Feu','SSR',['DPS', 'STUN'],'Obito.png');
+var Onoki = createNinja('Onoki',600,'I','Terre','Terre','SR',['AOE'],'Onoki.png');
+var Orochimaru = createNinja('Orochimaru',600,'I','Air','Air','SR',['AOE'],'Orochimaru.png');
 var Pein = createNinja('Tendo Pein',0,'A','God','Normal','SSR',['DPS'],'Pein.png');
-var Kushina = createNinja('Kushina Uzumaki',600,'I','Eau','Eau','SR',['SUPPORT'],'Kushina.png');
-var Deidara = createNinja('Deidara',600,'I','Terre','Terre','SR',['DPS'],'Deidara.png');
-var Hiruzen = createNinja('Hiruzen Sarutobi',600,'I','Terre','Eau','SR',['AOE'],'Hiruzen.png');
-var Konan = createNinja('Konan',800,'A','Eau','Terre','SR',['AOE'],'Konan.png');
+var Rin = createNinja('Rin',600,'I','Normal','Normal','R',['SUPPORT'],'Rin.png');
+var Rock = createNinja('Rock Lee',0,'A','Normal','Normal','R',['BURST'],'Rock.png');
+var Sakura = createNinja('Sakura Haruno',600,'I','Normal','Normal','R',['SUPPORT'],'Sakura.png');
+var Sasori = createNinja('Sasori',600,'A','Air','Air','SR',['AOE'],'Sasori.png');
+var Sasuke = createNinja('Sasuke Uchiha',0,'A','Foudre','Foudre','SR',['AOE'],'Sasuke.png');
+var Shikamaru = createNinja('Shikamaru Nara',600,'I','Terre','Terre','R',['STUN', 'SUPPORT'],'Shikamaru.png')
+var Shino = createNinja('Shino Aburame',600,'I','Terre','Terre','SR',['DPS'],'Shino.png');;
+var Shisui = createNinja('Shisui Uchiha',0,'A','Feu','Feu','SSR',['BURST'],'Shisui.png');
+var Suigetsu = createNinja('Suigetsu',0,'A','Eau','Eau','SR',['DPS', 'STUN'],'Suigetsu.png');
+var Temari = createNinja('Temari',600,'A','Air','Air','SR',['DPS'],'Temari.png');
+var Tenten = createNinja('Tenten',525,'A','Normal','Air','R',['DPS'],'Tenten.png');
+var Tobirama = createNinja('Tobirama Senju',0,'A','Eau','Eau','SR',['AOE'],'Tobirama.png');
+var Tsunade = createNinja('Tsunade',0,'F','Eau','Feu','SR',['TANK', 'STUN'],'Tsunade.png');
+var Zabuza = createNinja('Zabuza',0,'A','Eau','Eau','R',['DPS'],'Zabuza.png');
+var Zetsu = createNinja('Zetsu',600,'A','Terre','Terre','SR',['DPS', 'SUPPORT'],'Zetsu.png');
+}
 
 
 // Listes des combos
+{
 var PassingSnow = createCombo('Passing Snow', [Zabuza, Haku]);
 var Team15 = createCombo('Team 15', [Kiba, Hinata, Shino, Kurenai]);
 var Team10 = createCombo('Team 10', [Choji, Ino, Shikamaru, Asuma]);
@@ -146,8 +169,8 @@ var FiveKages = createCombo('Five Kages', [Onoki, Tsunade, Mei, Gaara, Ay]);
 var Tsuchi = createCombo('Tsuchikage Bodyguard', [Onoki, Kurotsuchi]);
 var JugosCage = createCombo('Jugo Cage', [Jugo, Kimimaro]);
 var SandTrio = createCombo('The Sand Trio', [Gaara, Temari, Kankuro]);
-var TailedBeast = createCombo('Tailed Beasts', [Killer, Narutos]);
-var Family = createCombo('Family', [Narutos, Minato, Kushina]);
+var TailedBeast = createCombo('Tailed Beasts', [Killer, Narutoj]);
+var Family = createCombo('Family', [Narutoj, Minato, Kushina]);
 var Mizu = createCombo('Mizukage Bodyguard', [Chojuro, Mei]);
 var Artistic = createCombo('Artistic', [Sasori, Deidara]);
 var WorkingShadow = createCombo('Working in the Shadow', [Zetsu, Kabuto, Madara, Obito, Pein]);
@@ -160,29 +183,10 @@ var Strongest = createCombo('Strongest Ninjas', [Hashirama, Madara]);
 var Uchiha = createCombo('Uchiha Clan', [Madara, Itachi, Shisui, Obito, Sasuke]);
 var Brothers = createCombo('Brothers', [Itachi, Sasuke]);
 var Rival = createCombo('Rival', [Maito, Kakashi]);
-var Prophecy = createCombo('Prophecy', [Narutoj, Jiraya]);
-var AspiringPeace = createCombo('Aspiring Peace', [Narutoj, Pein]);
+var Prophecy = createCombo('Prophecy', [Narutos, Jiraya]);
+var AspiringPeace = createCombo('Aspiring Peace', [Narutos, Pein]);
 var BestFriends = createCombo('Best of Friends', [Itachi, Shisui]);
 var Heavenly = createCombo('Heavenly Pained', [Pein, Konan]);
-
-// Affichage menu principal
-var compteur = 0;
-for (var i in ninjaList) {
-	var btn = document.createElement("input");
-	var div = document.getElementById("Page-Menu-Liste-Ninja");
-	btn.type = "image";
-	btn.src = ninjaList[i].src;
-	btn.value = ninjaList[i].nom;
-	btn.onclick = function() {
-		profilNinja(this);
-	}
-	div.appendChild(btn);
-	compteur++;
-	if (compteur == 8) {
-		var br = document.createElement("br");
-		div.appendChild(br);
-		compteur = 0;
-	}
 }
 
 // Fonction affiche profil d'un ninja
@@ -249,3 +253,172 @@ function createCombo(comboName, ninjalist) {
 	}
 	return combo;
 }
+
+// Fonction qui filtre les ninjas
+function filtre(type, click) {
+	// On update le filtre dans l'array
+	var index = filtreList.indexOf(type);
+	if (index == -1) {
+		if (click == 2) {
+			filtreList.push(type);
+		}
+		else {
+			filtreList = [type];
+		}
+	}
+	else {
+		filtreList.splice(index, 1);
+	}
+	// On update le texte indiquant les filtres utilisés
+	if (filtreList.length == 0) {
+		document.getElementById("Page-Menu-Filtres-Texte").innerHTML = 'Filtre: Aucun';
+	}
+	else {
+		var texte = '';
+		for (var i=0; i<filtreList.length; i++) {
+			texte = texte + filtreList[i] + ' ';
+		}
+		document.getElementById("Page-Menu-Filtres-Texte").innerHTML = 'Filtre: ' + texte;
+	}
+	// On clear la liste des ninjas
+	var div = document.getElementById("Page-Menu-Liste-Ninja");
+	div.innerHTML = "";
+	// On parcourt tout les ninjas et on ajoute si tout les filtres correspondent s'il y en a
+	if (filtreList.length == 0) {
+		afficherNinjaList();
+	}
+	else {
+		var compteur = 0;
+		var div = document.getElementById("Page-Menu-Liste-Ninja");
+		var btn;
+		for (var j in ninjaList) {
+			var bool = 1;
+			var boolrole = 0;
+			for (var k in filtreList) {
+				var tmpFiltre = typeFiltreDict[filtreList[k]];
+				if (tmpFiltre == 'mainstat') {
+					if (filtreDict[filtreList[k]] != ninjaList[j].mainstat) {
+						bool = 0;
+					}
+				}
+				else if (tmpFiltre == 'range') {
+					if (filtreList[k] == 'Ranged') {
+						if (ninjaList[j].range == 0) {
+							bool = 0;
+						}
+					}
+					else {
+						if (ninjaList[j].range > 0) {
+							bool = 0;
+						}
+					}
+				}
+				else if (tmpFiltre == 'rarity') {
+					if (filtreList[k] != ninjaList[j].rarity) {
+						bool = 0;
+					}
+				}
+				else if (tmpFiltre == 'role') {
+					for (var l in ninjaList[j].role) {
+						if (filtreList[k] == ninjaList[j].role[l]) {
+							boolrole = 1;
+						}
+					}
+					bool = boolrole;
+				}
+			}
+			if (bool == 1) {
+				btn = document.createElement("input");
+				btn.type = "image";
+				btn.src = ninjaList[j].src;
+				btn.value = ninjaList[j].nom;
+				btn.onclick = function() {
+					profilNinja(this);
+				}
+				div.appendChild(btn);
+				compteur++;
+				if (compteur == 8) {
+					var br = document.createElement("br");
+					div.appendChild(br);
+					compteur = 0;
+				}
+			}
+		}
+	}
+}
+
+// Fonction qui affiche tout les ninja dans le menu principal
+function afficherNinjaList() {
+	var compteur = 0;
+	var div = document.getElementById("Page-Menu-Liste-Ninja");
+	var btn;
+	for (var i in ninjaList) {
+		btn = document.createElement("input");
+		btn.type = "image";
+		btn.src = ninjaList[i].src;
+		btn.value = ninjaList[i].nom;
+		btn.onclick = function() {
+			profilNinja(this);
+		}
+		div.appendChild(btn);
+		compteur++;
+		if (compteur == 8) {
+			var br = document.createElement("br");
+			div.appendChild(br);
+			compteur = 0;
+		}
+	}
+}
+
+// Fonctions des boutons (hors ninja)
+$('input').mousedown(function(click){
+	var id = click.target.id;
+	if (id == "Page-Profil-Retour") {
+		changePage("Page-Menu");
+	}
+	else if (id == "Page-Menu-Filtre-Force") {
+		filtre('Force', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-Agil") {
+		filtre('Agilité', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-Intel") {
+		filtre('Intelligence', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-Ranged") {
+		filtre('Ranged', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-Melee") {
+		filtre('Melee', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-R") {
+		filtre('R', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-SR") {
+		filtre('SR', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-SSR") {
+		filtre('SSR', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-DPS") {
+		filtre('DPS', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-BURST") {
+		filtre('BURST', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-TANK") {
+		filtre('TANK', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-AOE") {
+		filtre('AOE', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-STUN") {
+		filtre('STUN', click.which);
+	}
+	else if (id == "Page-Menu-Filtre-SUPPORT") {
+		filtre('SUPPORT', click.which);
+	}
+});
+
+// Affichage du menu principal
+afficherNinjaList();
